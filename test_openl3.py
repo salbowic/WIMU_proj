e031ebe9c8d0ff1e1bcc51cc3790e50a6f999f9d
salbowic <s.albowicz@gmail.com>
2024-12-22 22:53:01 +0100

    modified:   test_openl3.py

--- test_openl3.py
+++ test_openl3.py
@@ -1,34 +1,7 @@
 from openl3_gtzan import EmbeddingVisualizer
 import os
 
-def main():
-    for i, file in enumerate(sampled_files):
-        file_path = os.path.join(genre_folder, file)
-        output_dir = f'results/embeddings/{genre}'
-        os.makedirs(output_dir, exist_ok=True)
-        
-
-        EmbeddingVisualizer.generate_embeddings()
-
-        # Update progress
-        sys.stdout.write(f"\rProcessed {i + 1}/{total_files} files from genre: {genre}")
-        sys.stdout.flush()
-
-        print()
-
-
-
-if __name__ == "__main__":
-    genres = [genre for genre in os.listdir(dataset_folder) if os.path.isdir(os.path.join(dataset_folder, genre))]
-    print(f"Found genres: {genres}")
-    for genre in genres:
-    genre_folder = os.path.join(dataset_folder, genre)
-    files = [file for file in os.listdir(genre_folder) if file.endswith(('.wav', '.ogg', '.flac'))]
-        # Randomly sample files from each genre
-    random.shuffle(files)
-    sampled_files = files[:num_samples_per_genre]
-    total_files = len(sampled_files)
-                
+if __name__ == "__main__":           
     # Initialize the visualizer with the dataset folder
     dataset_folder = "gtzan_dataset/genres_original"
     visualizer = EmbeddingVisualizer(dataset_folder)
